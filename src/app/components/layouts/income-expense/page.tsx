b263a11f72c12dd409483b6f2f98f1895853e9c0
tomao112 <toma01123719@gmail.com>
2024-08-27 23:25:53 +0900

    カレンダーに入力した値をtransactionListに即座に反映させるように

--- src/app/components/layouts/income-expense/page.tsx
+++ src/app/components/layouts/income-expense/page.tsx
@@ -1,18 +1,20 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { supabase } from '@/app/lib/supabaseClient';
+import { supabase } from '@/app/lib/supabaseClient'
 import type { Transaction } from './transactions';
 import Calendar from '@/app/components/calendar/calendar';
 import TransactionList from '@/app/components/layouts/income-expense/TransactionList';
 import TransactionForm from '@/app/components/layouts/income-expense/TransactionForm';
 
 export default function Home() {
+  // transactionsデータとカレンダーで選択された日付を管理する
   const [transactions, setTransactions] = useState<Transaction[]>([]);
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
 
   useEffect(() => {
     const fetchTransactions = async () => {
+      // transactionsテーブルのdateカラムを降順で取得
       const { data, error } = await supabase
         .from('transactions')
         .select('*')
@@ -21,12 +23,41 @@ export default function Home() {
       if (error) {
         console.error('Error fetching transactions:', error);
       } else {
-        console.log('Fetched transactions:', data);
+        // console.log('Fetched transactions:', data);
         setTransactions(data as Transaction[]);
       }
     };
 
     fetchTransactions();
+    const subscription = supabase
+    .channel('public:transactions')
+    .on('postgres_changes', { event: '*', schema: 'public', table: 'transactions' }, (payload) => {
+      console.log('Change received!', payload);
+
+      const newTransaction = payload.new as Transaction;
+      const oldTransaction = payload.old as Transaction;
+
+      switch (payload.eventType) {
+        case 'INSERT':
+          setTransactions(prevTransactions => [newTransaction, ...prevTransactions]);
+          break;
+        case 'UPDATE':
+          setTransactions(prevTransactions =>
+            prevTransactions.map(t => t.id === newTransaction.id ? newTransaction : t)
+          );
+          break;
+        case 'DELETE':
+          setTransactions(prevTransactions =>
+            prevTransactions.filter(t => t.id !== oldTransaction.id)
+          );
+          break;
+      }
+    })
+    .subscribe();
+
+  return () => {
+    supabase.removeChannel(subscription);
+  };
   }, []);
 
   const handleAddTransaction = async (newTransaction: {

730542dfff5eb7a0f4fd014b6cae4ab34a734b02
tomao112 <toma01123719@gmail.com>
2024-10-11 22:51:42 +0900

    speeddialコメントアウト作成

--- src/app/components/layouts/Chart/speedDial.tsx
+++ src/app/components/layouts/Chart/speedDial.tsx
@@ -1,17 +1,18 @@
+// speedDialComponents
 import React, { useRef } from 'react';
 import { SpeedDial } from 'primereact/speeddial';
-import { Toast } from 'primereact/toast';
-import { MenuItem } from 'primereact/menuitem';
-import { useRouter } from 'next/router';
+import { Toast } from 'primereact/toast'; //ユーザーに通知を表示
+import { MenuItem } from 'primereact/menuitem';  
+// import { useRouter } from 'next/router';
 
-export default function SpeedDialComponent() { // コンポーネント名を修正
+export default function SpeedDialComponent() {
     const toast = useRef<Toast>(null);
     // const router = useRouter();
     const items: MenuItem[] = [
         {
-            label: 'Add',
-            icon: 'pi pi-pencil',
-            command: () => {
+            label: 'Add',  //表示名
+            icon: 'pi pi-pencil',  //アイコン
+            command: () => {  //クリック時の実行換数
                 toast.current?.show({ severity: 'info', summary: 'Add', detail: 'Data Added' });
             }
         },
@@ -27,7 +28,7 @@ export default function SpeedDialComponent() { // コンポーネント名を修
     return (
         <div className="card">
             <div style={{ position: 'relative' }}>
-              <SpeedDial model={items} direction="right" style={{ position: 'absolute', top: '-1.2rem', right: '2rem'  }} />
+                <SpeedDial model={items} direction="right" style={{ position: 'absolute', top: '-1.2rem', right: '2rem'  }} />
             </div>
         </div>
     );

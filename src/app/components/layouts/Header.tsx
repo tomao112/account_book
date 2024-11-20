f3a78d4ee8bee57ffd64aa31b4af6b9a364b080e
tomao112 <toma01123719@gmail.com>
2024-11-21 00:02:05 +0900

    update: sidebarのレイアウトを再調整 headerにテスト用リンクを挿入

--- src/app/components/layouts/Header.tsx
+++ src/app/components/layouts/Header.tsx
@@ -9,14 +9,15 @@ const Header = () => {
 		// ヘッダーにアイコン、ハンバーガーを表示させるように
 		<div>
 			<div className='bg-gray-100 pt-3 pb-3  grid grid-cols-1'>
-				<Headless />
 				<div className='flex justify-between'>
+					<Link href={"/components/layouts"}>テスト</Link>
 					<Button style={{ color: 'black', fontSize: '2rem' }} icon="pi pi-home" rounded aria-label="Filter" />
 					<div className='flex items-center'>
 						<Button>会員</Button>
 					</div>
 				</div>
 			</div>
+			<Headless />
 		</div>
 	)
 }
--- src/app/components/layouts/sidebar.tsx
+++ src/app/components/layouts/sidebar.tsx
@@ -1,83 +1,30 @@
 // account_book/src/app/components/layouts/sidebar.tsx
 'use client'
-import React, { useState, useRef } from 'react';
-import { Sidebar } from 'primereact/sidebar';
-import { Button } from 'primereact/button';
-// import { Avatar } from 'primereact/avatar';
-// import { Ripple } from 'primereact/ripple';
-// import { StyleClass } from 'primereact/styleclass';
-import 'primereact/resources/themes/saga-blue/theme.css'; // テーマ
-import 'primereact/resources/primereact.min.css'; // PrimeReactのスタイル
+import React, { useState, useRef, useEffect } from 'react';
 import 'primeicons/primeicons.css';
+import { Tooltip } from 'primereact/tooltip';
+import TransactionList from './Chart/page';
 import Link from 'next/link';
-        
 
 export default function Headless() {
-    const [visible, setVisible] = useState(false);
-		const [ currentPage, setCurrentPage ] = useState('/');
-    // const btnRef1 = useRef(null);
-    // const btnRef2 = useRef(null);
-    // const btnRef3 = useRef(null);
-    // const btnRef4 = useRef(null);
 
     return (
-      <div className="card flex justify-content-center">
-        <Button icon="pi pi-bars" onClick={() => setVisible(true)} />
-				<Sidebar
-				className='w-16rem'
-					visible={visible}
-					onHide={() => setVisible(false)}
-					content={({ closeIconRef, hide }) => (
-        <div className="w-56">
-            <div id="app-sidebar-2" className="">
-                <div className="">
-									{/* 閉じるボタン */}
-                    {/* <div className="flex justify-end pr-3 pt-3">
-                      <Button type="button" onClick={(e) => hide(e)} icon="pi pi-times" rounded outlined className="h-2rem w-2rem"></Button>
-                    </div> */}
-										<ul className='pl-10 pt-10 flex flex-col gap-2'>
-											<Link href="./TransactionList">
-												<li className='' style={{ backgroundColor: currentPage ===  './TransactionList' ? 'var(--highlight-bg)' : 'white', color: 'var(--highlight-text-color)', borderRadius: '0.7rem', padding: '0.7rem' }}
-												onClick={() => setCurrentPage('./TransactionList')}>
-													<Button className='text-xl flex gap-4' >
-														<span className='pi pi-book' style={{ fontSize: '1.5rem' }}></span>
-														<span>日別</span>
-													</Button>
-												</li>
-											</Link>
-											<Link href="./income-expense">
-												<li style={{ backgroundColor: currentPage ===  './income-expense' ?'var(--highlight-bg)': 'white', color: 'var(--highlight-text-color)', borderRadius: '0.7rem', padding: '0.7rem' }}
-												onClick={() => setCurrentPage('./income-expense')}>
-													<Button className='text-xl flex gap-4'>
-														<span className='pi pi-calendar' style={{ fontSize: '1.5rem' }}></span>
-														<span>カレンダー</span>
-													</Button>
-												</li>
-											</Link>
-											<Link href="./Chart">
-												<li style={{ backgroundColor: currentPage ===  './Chart' ? 'var(--highlight-bg)': 'white', color: 'var(--highlight-text-color)', borderRadius: '0.7rem', padding: '0.7rem' }}
-												onClick={() => setCurrentPage('./Chart')}>
-													<Button className='text-xl flex gap-4'>
-														<span className='pi pi-chart-line' style={{ fontSize: '1.5rem' }}></span>
-														<span>チャート</span>
-													</Button>
-												</li>
-											</Link>
-											<Link href="">
-												<li style={{ backgroundColor: currentPage ===  './setting' ?'var(--highlight-bg)': 'white', color: 'var(--highlight-text-color)', borderRadius: '0.7rem', padding: '0.7rem' }}
-												onClick={() => setCurrentPage('./Setting')}>
-													<Button className='text-xl flex gap-4'>
-														<span className='pi pi-cog' style={{ fontSize: '1.5rem' }}></span>
-														<span>設定</span>
-													</Button>
-												</li>
-											</Link>
-										</ul>
-                </div>
+        <div className='w-20 flex flex-col'>
+            <Tooltip target=".tooltip" className="tooltip-custom" />
+            <div className='flex items-center justify-center flex-col gap-5'>
+							<Link className='tooltip w-14 h-14 flex items-center justify-center' data-pr-tooltip="日別" href={"./TransactionList"}>
+                <span className='pi pi-book mt-2' style={{ fontSize: '1.75rem' }}></span>
+							</Link>
+							<Link className="tooltip w-14 h-14 flex items-center justify-center" data-pr-tooltip="カレンダー" href={"./income-expense"}>
+								<span className='pi pi-calendar mt-2' style={{ fontSize: '1.75rem' }}></span>
+							</Link>
+							<Link className="tooltip w-14 h-14 flex items-center justify-center" data-pr-tooltip="チャート" href={"./Chart"}>
+								<span className='pi pi-chart-line mt-2' style={{ fontSize: '1.75rem' }}></span>
+							</Link>
+							<Link className="tooltip w-14 h-14 flex items-center justify-center" data-pr-tooltip="設定" href={"./"}>
+								<span className='pi pi-cog mt-2' style={{ fontSize: '1.75rem' }}></span>
+							</Link>
             </div>
         </div>
-    )}
-></Sidebar>
-      </div>
     )
 }

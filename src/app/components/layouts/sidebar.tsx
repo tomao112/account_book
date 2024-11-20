aaaa5e46cd3c820eba769fc7be27e7d36d8e06f9
tomao112 <toma01123719@gmail.com>
2024-11-21 00:39:28 +0900

    update: tooltipの文言を修正、アイコンのカラーを修正

--- src/app/components/layouts/sidebar.tsx
+++ src/app/components/layouts/sidebar.tsx
@@ -12,17 +12,17 @@ export default function Headless() {
         <div className='w-20 flex flex-col'>
             <Tooltip target=".tooltip" className="tooltip-custom" />
             <div className='flex items-center justify-center flex-col gap-5'>
-							<Link className='tooltip w-14 h-14 flex items-center justify-center' data-pr-tooltip="日別" href={"./TransactionList"}>
-                <span className='pi pi-book mt-2' style={{ fontSize: '1.75rem' }}></span>
+							<Link className='tooltip w-14 h-14 flex items-center justify-center' data-pr-tooltip="daily" href={"./TransactionList"}>
+                <span className='pi pi-book mt-2' style={{ fontSize: '1.75rem', color: '#495057' }}></span>
 							</Link>
-							<Link className="tooltip w-14 h-14 flex items-center justify-center" data-pr-tooltip="カレンダー" href={"./income-expense"}>
-								<span className='pi pi-calendar mt-2' style={{ fontSize: '1.75rem' }}></span>
+							<Link className="tooltip w-14 h-14 flex items-center justify-center" data-pr-tooltip="calendar" href={"./income-expense"}>
+								<span className='pi pi-calendar mt-2' style={{ fontSize: '1.75rem', color: '#495057' }}></span>
 							</Link>
-							<Link className="tooltip w-14 h-14 flex items-center justify-center" data-pr-tooltip="チャート" href={"./Chart"}>
-								<span className='pi pi-chart-line mt-2' style={{ fontSize: '1.75rem' }}></span>
+							<Link className="tooltip w-14 h-14 flex items-center justify-center" data-pr-tooltip="chart" href={"./Chart"}>
+								<span className='pi pi-chart-line mt-2' style={{ fontSize: '1.75rem', color: '#495057' }}></span>
 							</Link>
-							<Link className="tooltip w-14 h-14 flex items-center justify-center" data-pr-tooltip="設定" href={"./"}>
-								<span className='pi pi-cog mt-2' style={{ fontSize: '1.75rem' }}></span>
+							<Link className="tooltip w-14 h-14 flex items-center justify-center" data-pr-tooltip="setting" href={"./"}>
+								<span className='pi pi-cog mt-2' style={{ fontSize: '1.75rem', color: '#495057' }}></span>
 							</Link>
             </div>
         </div>

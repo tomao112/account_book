8cef966f64626430431ce77ff96df58b0b24cc43
tomao112 <toma01123719@gmail.com>
2024-11-21 23:27:02 +0900

    update: headerレイアウト調整 画像追加

--- src/app/components/layouts/Header.tsx
+++ src/app/components/layouts/Header.tsx
@@ -2,18 +2,35 @@ import Link from 'next/link';
 import React from 'react';
 import { Button } from 'primereact/button';
 import Headless from '@/app/components/layouts/sidebar';
+import Image from "next/image"
+
 
 const Header = () => {
 	return (
-		// デザイン考案中
-		// ヘッダーにアイコン、ハンバーガーを表示させるように
-		<div>
-			<div className='bg-gray-100 pt-3 pb-3  grid grid-cols-1'>
+		<div className='h-20'>
+			<div className='pt-3 pb-3 pl-3 pr-10 grid grid-cols-1'>
 				<div className='flex justify-between'>
-					<Link href={"/components/layouts"}>テスト</Link>
-					<Button style={{ color: 'black', fontSize: '2rem' }} icon="pi pi-home" rounded aria-label="Filter" />
+					{/* ホームアイコンもサイドバーに追加予定 */}
+					{/* <Button style={{ color: 'black', fontSize: '2rem' }} icon="pi pi-home" rounded aria-label="Filter" /> */}
+					<Image 
+						src="/header_hum.jpg"
+						width={60}
+						height={60}
+						alt="はむ"
+					/>
 					<div className='flex items-center'>
-						<Button>会員</Button>
+						<Button className='gap-2'>
+							<Image
+								src="/icon_hum.jpg"
+								width={40}
+								height={40}
+								alt="icon"
+								/>
+								<span>
+									register
+								</span>
+								<i className='pi pi-angle-down'></i>
+						</Button>
 					</div>
 				</div>
 			</div>

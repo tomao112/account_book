8c4d7335bf14ecc3c402a09885c4aee7e5fdfe51
tomao112 <toma01123719@gmail.com>
2024-11-22 14:51:56 +0900

    add: virtualscroll実装の為テストファイルを追加

---
+++ src/app/components/layouts/TransactionList/VirtualScroll.tsx
@@ -0,0 +1,47 @@
+
+import React, { useState } from 'react';
+import { VirtualScroller, VirtualScrollerTemplateOptions } from 'primereact/virtualscroller';
+import { Skeleton } from 'primereact/skeleton';
+import { classNames } from 'primereact/utils';
+
+export default function LoadingDemo() {
+    const [items] = useState<string[]>(Array.from({ length: 100000 }).map((_, i) => `Item #${i}`));
+
+    const itemTemplate = (item: string, options: VirtualScrollerTemplateOptions) => {
+        const className = classNames('flex align-items-center p-2', {
+            'surface-hover': options.odd
+        });
+
+        return (
+            <div className={className} style={{ height: options.props.itemSize + 'px' }}>
+                {item}
+            </div>
+        );
+    };
+
+    const loadingTemplate = (options: VirtualScrollerTemplateOptions) => {
+        const className = classNames('flex align-items-center p-2', {
+            odd: options.odd
+        });
+
+        return (
+            <div className={className} style={{ height: '50px' }}>
+                <Skeleton width={options.even ? '60%' : '50%'} height="1.3rem" />
+            </div>
+        );
+    };
+
+    return ( 
+        <div className="card flex flex-wrap justify-content-center gap-5">
+            <div>
+                <span className="font-bold block mb-2">Modal</span>
+                <VirtualScroller items={items} itemSize={50} itemTemplate={itemTemplate} showLoader delay={250} className="border-1 surface-border border-round" style={{ width: '200px', height: '200px' }} />
+            </div>
+            <div>
+                <span className="font-bold block mb-2">Skeleton</span>
+                <VirtualScroller items={items} itemSize={50} itemTemplate={itemTemplate} showLoader delay={250} loadingTemplate={loadingTemplate} className="border-1 surface-border border-round" style={{ width: '200px', height: '200px' }} />
+            </div>
+        </div>
+    );
+}
+        

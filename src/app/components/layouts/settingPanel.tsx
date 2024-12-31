3fcf20b0885f1edd611196f0d274805bde517256
toma3719 <toma01123719@gmail.com>
2025-01-01 01:14:23 +0900

    update: 設定ページのz-indexを修正

--- src/app/components/layouts/settingPanel.tsx
+++ src/app/components/layouts/settingPanel.tsx
@@ -10,7 +10,7 @@ interface SettingPanelProps {
 
 const SettingPanel: React.FC<SettingPanelProps> = ({ isOpen, onContactClick }) => {
     return (
-        <div className={`flex item fixed z-50 top-0 right-0 h-full w-[90%] bg-white shadow-lg transition-transform duration-300 ${isOpen ? 'translate-x-0' : 'translate-x-full'}`}>
+        <div className={`flex item fixed z-40 top-0 right-0 h-full w-[90%] bg-white shadow-lg transition-transform duration-300 ${isOpen ? 'translate-x-0' : 'translate-x-full'}`}>
             <div className='flex flex-col border'>
               <ul className='flex flex-col pt-10 gap-3 w-[20rem] items-center'>
                 <li><button onClick={onContactClick} className='border-b w-[8rem] pb-3'>お問い合わせ</button></li>
